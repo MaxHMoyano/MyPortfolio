5f3de52313ee88d40094e6b99b1589e380bdc751
Max Moyano <maximiliano.moyano@corbisstudio.com>
2020-06-24 16:03:14 -0300

    added skills

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Handler when the DOM is fully loaded
   let buttons = document.querySelectorAll(
-    "#work_btn, #about_btn, #contact_btn"
+    '#work_btn, #skills_btn, #contact_btn'
   );
-  buttons.forEach(btn => {
-    btn.addEventListener("click", e => {
-      buttons.forEach(btn => btn.classList.remove("active"));
-      btn.classList.add("active");
-      document.location = `#${btn.id.split("_")[0]}`;
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      buttons.forEach((btn) => btn.classList.remove('active'));
+      btn.classList.add('active');
+      document.location = `#${btn.id.split('_')[0]}`;
     });
   });
 });
 
 function goToHome() {
   let buttons = document.querySelectorAll(
-    "#work_btn, #about_btn, #contact_btn"
+    '#work_btn, #skills_btn, #contact_btn'
   );
-  buttons.forEach(btn => btn.classList.remove("active"));
-  document.location = "#home";
+  buttons.forEach((btn) => btn.classList.remove('active'));
+  document.location = '#home';
 }
